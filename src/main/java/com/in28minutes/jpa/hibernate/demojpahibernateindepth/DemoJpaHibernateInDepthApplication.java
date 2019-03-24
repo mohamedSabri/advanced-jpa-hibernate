c03191075be5d0315280146e7ce55462f71a4ef8
mohamed_sabri <mohamed.sabri@visionvalley.net>
2019-03-24 13:29:17 +0200

    first commit

---
+++ src/main/java/com/in28minutes/jpa/hibernate/demojpahibernateindepth/DemoJpaHibernateInDepthApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.jpa.hibernate.demojpahibernateindepth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJpaHibernateInDepthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJpaHibernateInDepthApplication.class, args);
+	}
+
+}
