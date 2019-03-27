bd93573f8f7caddf431453c2f23d0cc4fbce1437
mohamed_sabri <mohamed.sabri@visionvalley.net>
2019-03-27 09:30:29 +0200

    Step 15 - JPQL - Basics

--- src/main/java/com/in28minutes/jpa/hibernate/demojpahibernateindepth/entity/Course.java
+++ src/main/java/com/in28minutes/jpa/hibernate/demojpahibernateindepth/entity/Course.java
@@ -3,8 +3,10 @@ package com.in28minutes.jpa.hibernate.demojpahibernateindepth.entity;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="Course")
 public class Course {
 
 	@Id
